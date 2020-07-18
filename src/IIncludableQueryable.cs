e596f02c327e69bebfa0ea790644874820751f0e
Kenneth Carter <kccarter32@gmail.com>
2020-07-17 18:14:43 -0600

    #53 added IIncludableQueryable to faciliate ThenbyMethod functionality

---
+++ src/IIncludableQueryable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SubSonic
+{
+    public interface IIncludableQueryable<out TEntity, out Property>
+        : IQueryable<TEntity>, IEnumerable<TEntity>, IQueryable, IEnumerable
+    {
+    }
+}
