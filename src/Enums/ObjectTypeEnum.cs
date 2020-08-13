d7c0f4e94762e0f24f920d782379bb0f2d3f99c4
Kenneth Carter <kccarter32@gmail.com>
2020-08-12 22:35:32 -0600

    commit next version of abstraction

---
+++ src/Enums/ObjectTypeEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SubSonic.Schema
+{
+    public enum DbObjectTypeEnum
+    {
+        Table = 0,
+        TableType,
+        View,
+        Procedure,
+        ScalarFunction,
+        TableFunction
+    }
+}
